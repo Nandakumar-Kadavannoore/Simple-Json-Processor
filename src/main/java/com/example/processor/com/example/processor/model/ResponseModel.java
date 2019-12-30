feeb712e7fb5f3a5718ee780974fbb7a2c6c565d
Nandakumar K <nandakumark6693@gmail.com>
2019-12-30 13:00:17 +0530

    Add Base response class
    Base response class added

---
+++ src/main/java/com/example/processor/com/example/processor/model/ResponseModel.java
@@ -0,0 +1,29 @@
+/*
+  @Copyright 2019-2020
+ */
+package com.example.processor.com.example.processor.model;
+
+import lombok.Builder;
+import lombok.Data;
+
+/**
+ * Model class for response.
+ * <p>
+ *     For all response, this model class is
+ *     return.
+ * </p>
+ */
+@Data
+@Builder
+public class ResponseModel {
+
+    /**
+     * Store response message.
+     */
+    private String message;
+
+    /**
+     * Store response object.
+     */
+    private Object object;
+}
