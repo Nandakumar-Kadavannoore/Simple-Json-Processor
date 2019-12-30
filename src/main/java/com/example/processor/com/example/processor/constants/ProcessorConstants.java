8e98e66b9d7bdb529c2518c6840e5e3af7508773
Nandakumar K <nandakumark6693@gmail.com>
2019-12-30 13:24:13 +0530

    Add application constants
    Grouped all constants values used in application in single file

---
+++ src/main/java/com/example/processor/com/example/processor/constants/ProcessorConstants.java
@@ -0,0 +1,26 @@
+/*
+  @Copyright 2019-2020
+ */
+package com.example.processor.com.example.processor.constants;
+
+/**
+ * Constants used in this application.
+ * @author com.example
+ */
+public  class ProcessorConstants {
+
+    /**
+     * Constant to store Success response message.
+     */
+    public static final String SUCCESS_RESPONSE_MESSAGE = "Success";
+
+    /**
+     * Constant to store Failure response message.
+     */
+    public static final String FAILURE_RESPONSE_MESSAGE = "Failure";
+
+    /**
+     * Constant to store record unique id.
+     */
+    public static final String RECORD_UNIQUE_ID_KEY = "_id";
+}
